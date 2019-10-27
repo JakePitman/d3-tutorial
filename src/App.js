6d6235bbcf9b29c41073e6d9e69a5dbe52afb55f
Jay Pitman <jakeseanp@gmail.com>
2019-10-27 21:42:32 +1100

    Add fundamental bar chart example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as d3 from "d3"
 import './App.css';
+import data from './data.csv'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+
+    const svg = d3.select('svg')
+
+    const width = +(svg.attr('width'))
+    const height = +(svg.attr('height'))
+
+    const render = (data) => {
+      const xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.population)])
+        .range([0, width])
+
+      const yScale = d3.scaleBand()
+        .domain(data.map(d => d.country))
+        .range([0, height])
+
+      svg.selectAll('rect').data(data)
+        .enter().append('rect')
+          .attr('y', d => yScale(d.country))
+          .attr('width', d => xScale(d.population))
+          .attr('height', yScale.bandwidth())
+    }
+
+    d3.csv(data).then(function(data) {
+      const parsedData = data.map(entry => {
+        return { ...entry, population: +(entry.population) }
+      })
+      render(parsedData)
+    });
+
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <h1>-- d3 --</h1>
+        <svg height="500" width="1000"></svg>
+      </div>
+    )
+  }
 }
 
 export default App;
